db022d1ceea1bfd0ca9a9a10d716db3af168e7aa
barry <nicolai.bernhard03@gmail.com>
2022-10-27 19:40:08 +0200

    unused imports

--- src/installers/defaults.rs
+++ src/installers/defaults.rs
@@ -1,8 +1,6 @@
 // Function to add default addons to the installer
 // Path to addons: ../../template/addons/index.ts
 extern crate fs_extra;
-use fs_extra::dir::{copy, CopyOptions};
-use std::fs;
 
 use crate::helpers::copy::copy_files;
 
