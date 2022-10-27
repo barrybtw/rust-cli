ed614ae35dccc5adbc153ad338c35802142d60a0
barry <nicolai.bernhard03@gmail.com>
2022-10-27 19:39:50 +0200

    extra

--- src/installers/defaults.rs
+++ src/installers/defaults.rs
@@ -4,20 +4,8 @@ extern crate fs_extra;
 use fs_extra::dir::{copy, CopyOptions};
 use std::fs;
 
+use crate::helpers::copy::copy_files;
+
 pub fn install_defaults(_app_name: &str) {
-    let options = CopyOptions::new(); //Initialize default values for CopyOptions
-
-    // check if directory app_name exists
-    // if it does, ask if they want to overwrite it
-    // if it doesn't, create it
-    if fs::metadata(_app_name).is_ok() {
-        println!("Directory {} already exists.", _app_name);
-    } else {
-        println!("Creating directory {}.", _app_name);
-        fs::create_dir(_app_name).expect("Failed to create directory.");
-
-        let source = "./src/template/base";
-        let destination = &_app_name;
-        copy(&source, &destination, &options).expect("Failed to copy directory.");
-    }
+    copy_files(&_app_name)
 }
